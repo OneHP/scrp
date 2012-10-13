c9a722219c70e303b0c044e6f6c092a3c67f33f0
Thomas Inman <onehitpoint@gmail.com>
2012-10-13 10:33:23 +0100

    Make web request

--- src/main/java/uk/co/onehp/service/ScheduledServiceImpl.java
+++ src/main/java/uk/co/onehp/service/ScheduledServiceImpl.java
@@ -2,18 +2,26 @@ package uk.co.onehp.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestOperations;
 
 @Service("scheduledService")
 public class ScheduledServiceImpl implements ScheduledService {
 
 	static final Logger LOG = LoggerFactory.getLogger(ScheduledServiceImpl.class);
 
+	@Autowired
+	RestOperations restOperations;
+
 	@Override
 	@Scheduled(cron = "* * * * * *")
 	public void scrape() {
 		LOG.info("scrape");
+		String response = this.restOperations.getForObject("http://qrym5z41qhtr19k9.browserver.org", String.class,
+				new Object[] {});
+		LOG.info(response);
 	}
 
 }
