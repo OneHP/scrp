5ae356d4203a12e7253f2656b8c1d68eed4c2493
Thomas Inman <onehitpoint@gmail.com>
2012-10-13 11:07:16 +0100

    Match given pattern

--- src/main/java/uk/co/onehp/service/ScheduledServiceImpl.java
+++ src/main/java/uk/co/onehp/service/ScheduledServiceImpl.java
@@ -1,5 +1,8 @@
 package uk.co.onehp.service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +18,19 @@ public class ScheduledServiceImpl implements ScheduledService {
 	@Autowired
 	RestOperations restOperations;
 
+	// private final Pattern pattern =
+	// Pattern.compile("[bB][aA][rR][cC][lL][aA][yY]");
+	private final Pattern pattern = Pattern.compile("[hH][oO][lL][mM][aA][nN]");
+
 	@Override
 	@Scheduled(cron = "0 * * * * *")
 	public void scrape() {
 		LOG.info("scrape");
 		String response = this.restOperations.getForObject(
 				"http://www.justice.gov.uk/courts/court-lists/list-cause-rcj", String.class, new Object[] {});
-		LOG.info(response);
+
+		Matcher matcher = this.pattern.matcher(response);
+		LOG.info("" + matcher.find());
 	}
 
 }
